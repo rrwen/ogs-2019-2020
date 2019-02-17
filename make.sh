941812169403475cd4abd462d8286ee148531c49
R W <rrwen.dev@gmail.com>
2019-02-16 22:38:39 -0500

    Update proposal

--- make.sh
+++ make.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-xelatex richard_wen_2019_ogs_proposal -interaction=nonstopmode -halt-on-error
-bibtex richard_wen_2019_ogs_proposal
-xelatex richard_wen_2019_ogs_proposal -interaction=nonstopmode -halt-on-error
-xelatex richard_wen_2019_ogs_proposal -interaction=nonstopmode -halt-on-error
+mkdir -p log
+xelatex -output-directory=log -interaction=nonstopmode -halt-on-error richard_wen_2019_ogs_proposal
+biber log/richard_wen_2019_ogs_proposal
+xelatex -output-directory=log -interaction=nonstopmode -halt-on-error richard_wen_2019_ogs_proposal
+mv log/richard_wen_2019_ogs_proposal.pdf ./richard_wen_2019_ogs_proposal.pdf
