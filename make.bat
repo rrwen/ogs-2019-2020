e388157a32aa385ebde539c0a630b4162b6779ef
rwenite <init.r.w@gmail.com>
2019-02-19 17:03:27 -0500

    Update final proposal

--- make.bat
+++ make.bat
@@ -1,4 +1,3 @@
 xelatex richard_wen_2019_ogs_proposal -output-directory=log -interaction=nonstopmode -halt-on-error
-biber log/richard_wen_2019_ogs_proposal 
-xelatex richard_wen_2019_ogs_proposal  -aux-directory=log -interaction=nonstopmode -halt-on-error
+biber log/richard_wen_2019_ogs_proposal
 xelatex richard_wen_2019_ogs_proposal  -aux-directory=log -interaction=nonstopmode -halt-on-error
